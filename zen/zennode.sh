34c9904ba4fa40ef11663ca6adf967fa2c5b36c3
Timothy Messer <tmesser@verus.io>
2018-01-22 16:07:10 -0600

    Add zen

---
+++ zen/zennode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up environment, pull latest hush software, set config, start node
+
+apt-get update
+
+apt-get --assume-yes install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python zlib1g-dev wget bsdmainutils automake
+
+git clone https://github.com/zencashofficial/zen.git
+
+./zen/zcutil/build.sh -j$(nproc)
+
+./zen/zcutil/fetch-params.sh
+
+touch ~/.zen/zen.conf
+
+screen
+
+./zen/src/zend

cb6624619345b160d0f0c80b62074db9e59f117f
Zach Kelling <z@zeekay.io>
2018-02-10 16:01:39 -0600

    Update Dockerfile for btcg, add bitcoingold.conf

--- btcg/scripts/build.sh
+++ btcg/scripts/build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-git clone https://github.com/BTCGPU/BTCGPU
 
+# Clone btcg repo
+git clone https://github.com/BTCGPU/BTCGPU
 cd BTCGPU
 
-./autogen.sh
-./configure --disable-wallet --without-gui
+# Build deps
+cd depends
+make
+cd ..
 
+# Configure project
+./autogen.sh
+./configure --disable-wallet --without-gui --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+
+# Build btcg
 make
 make install
